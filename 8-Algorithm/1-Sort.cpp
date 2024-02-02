03c709472db22812eb5d16a3a0be59800e41d808
SK2iP <sadra.khanjari@gmail.com>
2024-02-02 12:08:56 +0330

    first commit

--- 8-Algorithm/1-Sort.cpp
+++ 8-Algorithm/1-Sort.cpp
@@ -22,59 +22,6 @@ void bubbleSort(vector<int>& arr) {
         }
     }
 }
-
-// Merge Sort Functions
-void merge(vector<int>& arr, int left, int middle, int right) {
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
-
-    vector<int> leftArray(n1);
-    vector<int> rightArray(n2);
-
-    for (int i = 0; i < n1; ++i) {
-        leftArray[i] = arr[left + i];
-    }
-    for (int j = 0; j < n2; ++j) {
-        rightArray[j] = arr[middle + 1 + j];
-    }
-
-    int i = 0;
-    int j = 0;
-    int k = left;
-
-    while (i < n1 && j < n2) {
-        if (leftArray[i] <= rightArray[j]) {
-            arr[k] = leftArray[i];
-            ++i;
-        } else {
-            arr[k] = rightArray[j];
-            ++j;
-        }
-        ++k;
-    }
-
-    while (i < n1) {
-        arr[k] = leftArray[i];
-        ++i;
-        ++k;
-    }
-
-    while (j < n2) {
-        arr[k] = rightArray[j];
-        ++j;
-        ++k;
-    }
-}
-
-void mergeSort(vector<int>& arr, int left, int right) {
-    if (left < right) {
-        int middle = left + (right - left) / 2;
-        mergeSort(arr, left, middle);
-        mergeSort(arr, middle + 1, right);
-        merge(arr, left, middle, right);
-    }
-}
-
 // Selection Sort Function
 void selectionSort(vector<int>& arr) {
     int n = arr.size();
@@ -102,13 +49,6 @@ int main() {
     cout << "\nAfter Bubble Sort: ";
     displayVector(numbers);
 
-    // Reset vector for the next sorting algorithm
-    numbers = {64, 25, 12, 22, 11};
-
-    // Merge Sort
-    mergeSort(numbers, 0, numbers.size() - 1);
-    cout << "\nAfter Merge Sort: ";
-    displayVector(numbers);
 
     // Reset vector for the next sorting algorithm
     numbers = {64, 25, 12, 22, 11};
